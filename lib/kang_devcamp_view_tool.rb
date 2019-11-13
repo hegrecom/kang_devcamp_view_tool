8baa0fb11c5fe47ae931c7cce7fe90352b8b5ca2
Taehwa Kang <hegrecomm@gmail.com>
2019-11-13 19:28:35 +0900

    Initial commit for my gem

---
+++ lib/kang_devcamp_view_tool.rb
@@ -0,0 +1,7 @@
+require "kang_devcamp_view_tool/version"
+require "kang_devcamp_view_tool/renderer"
+
+module KangDevcampViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/kang_devcamp_view_tool/version.rb
@@ -0,0 +1,3 @@
+module KangDevcampViewTool
+  VERSION = "0.1.0"
+end
